0acd17fd444192120313b912ad8d879c888fa8da
Arjun Lall <lallarjun@gmail.com>
2017-06-05 16:54:07 +0000

    benchmarking yield

---
+++ benchmark.rb
@@ -0,0 +1,31 @@
+# start_time = Time.now
+
+# # Calculate the 200th Fibonacci number
+# fibonacci(200)
+
+# end_time = Time.now
+
+# # This will return the difference in the timestamps in seconds
+# running_time = end_time - start_time
+
+# puts "fibonacci(200) took #{running_time} seconds."
+
+
+
+
+def benchmark
+  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  end_time - start_time
+end
+
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100000000
+
+running_time = benchmark { long_string.reverse }
+
+puts "string.reverse took #{running_time} seconds to run"
+
